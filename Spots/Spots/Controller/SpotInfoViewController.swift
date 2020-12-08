48a311ebd0da9eea699e7bd5b8e95ae1dc347ab8
shannonysu <54588702+shannonysu@users.noreply.github.com>
2020-12-08 17:18:43 +0800

    Add files via upload
    saved tab initial commit

---
+++ Spots/Spots/Controller/SpotInfoViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SpotInfoViewController.swift
+//  Spots
+//
+//  Created by Shannon Su on 12/8/20.
+//  Copyright © 2020 SpotsDevelopers. All rights reserved.
+//
+
+import UIKit
+
+class SpotInfoViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var clickedSpot: Spot!
+    
+    
+}

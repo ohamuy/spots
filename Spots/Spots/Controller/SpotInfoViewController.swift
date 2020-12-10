802c9df64d3b550fac7549976ce02f7d195d0968
ohamuy <46689344+ohamuy@users.noreply.github.com>
2020-12-10 12:42:51 -0600

    Revert "- changed the add new pin button on the home page to pin current location"
    This reverts commit 6122971ad36cc84f37d5ac7094de9b057fa3fa61.

--- Spots/Spots/Controller/SpotInfoViewController.swift
+++ Spots/Spots/Controller/SpotInfoViewController.swift
@@ -9,15 +9,11 @@
 import UIKit
 import MapKit
 
-class SpotInfoViewController: UIViewController{
+class SpotInfoViewController: UIViewController, MapKit{
     
-    @IBOutlet var mapView: MKMapView!
     var clickedSpot: Spot!
     
     override func viewDidLoad() {
-        let latitude = clickedSpot.coordinate.latitude
-        let longitude = clickedSpot.coordinate.longitude
-        let theSpot = Spot(label: clickedSpot.label!, locationName: clickedSpot.locationName, category: clickedSpot.category, coordinate: CLLocationCoordinate2DMake(latitude, longitude))
-        mapView.addAnnotation(theSpot)
+        <#code#>
     }
 }

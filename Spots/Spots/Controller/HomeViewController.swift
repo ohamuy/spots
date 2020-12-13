667e96540f41af5dc6628657b3337e796557bcfd
Kathryn Krumholz <k.krumholz@wustl.edu>
2020-12-13 09:59:37 -0600

    updated map

--- Spots/Spots/Controller/HomeViewController.swift
+++ Spots/Spots/Controller/HomeViewController.swift
@@ -8,33 +8,154 @@
 
 import UIKit
 import MapKit
+import FirebaseStorage
+import FirebaseAuth
+import Firebase
 
-class HomeViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegate, UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+class HomeViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegate, UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate, CLLocationManagerDelegate {
 
+    
+
+    
+    @IBOutlet var locationMapView: MKMapView!
+   
+    @IBOutlet weak var search: UISearchBar!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         search.delegate = self
         search.showsCancelButton = true
 
+        checkLocationServices()
+       // updateMap()
     }
     
-    @IBOutlet var mapView: MKMapView!
-   
-    @IBOutlet weak var search: UISearchBar!
+    override func viewDidAppear(_ animated: Bool) {
+        updateMap()
+    }
     
+    let db = Firestore.firestore()
     var theData: APIResults?
     var searchInput: String = ""
     var newSearchInput: String = ""
     let APIKey = "AIzaSyDrjVeQhWVpIJhYrrVX9vyykLhq475jjkY"
-    let myArray: NSArray = ["First","Second","Third"]
     var myTableView: UITableView!
+
+    let manager = CLLocationManager()
+    let regionMeter: Double = 10000
+    var userPins: [[String: Any]] = []
  
+    //Set up location manager
+    func setUpLocationManager() {
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    //populates map with spots from firestore
+    func updateMap() {
+        if Auth.auth().currentUser != nil{
+              self.userPins.removeAll()
+              let uid = Auth.auth().currentUser!.uid
+        
+              db.collection("spots").whereField("uid", isEqualTo: uid)
+              .getDocuments() { (querySnapshot, err) in
+                  if let err = err {
+                      print("Error getting documents: \(err)")
+                  } else {
+                      for document in querySnapshot!.documents {
+                          //print("\(document.documentID) => \(document.data())")
+                          self.userPins.append(document.data())
+                      }
+                      print(self.userPins)
+                      print(self.userPins.count)
+                    for index in 0 ..< self.userPins.count  {
+                        //print("\(index) : \(self.userPins[index]["title"] ?? <#default value#>)")
+                        
+                              let annotation = MKPointAnnotation()
+                        let centerCoordinate = CLLocationCoordinate2D(latitude: self.userPins[index]["latitude"] as! CLLocationDegrees, longitude: self.userPins[index]["longitude"] as! CLLocationDegrees)
+                              annotation.coordinate = centerCoordinate
+                        annotation.title = "\(self.userPins[index]["title"] ?? "")"
+                        self.locationMapView.addAnnotation(annotation)
+                          }
+                  }
+              }
+          }
+    }
+    
+    //Check if location services are available
+    func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled() {
+            //Set Up location manager
+            setUpLocationManager()
+            checkAppLocationAuthorization()
+        } else {
+            let alert = UIAlertController(title: "Location Service Not Enabled", message: "Please enable location services to improve user experience", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        }
+    }
+    
+    //Check Location services permission for the app
+    func checkAppLocationAuthorization() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            //Show user location on map
+            locationMapView.showsUserLocation = true
+            scaleMapToUserLocation()
+            manager.startUpdatingLocation()
+            break
+        case .denied:
+            //Show alert instructing how to enable them within settings
+            break
+        case .notDetermined:
+            manager.requestWhenInUseAuthorization()
+            break
+        case .restricted:
+            //Show alert that location is restricted
+            break
+        case .authorizedAlways:
+            break
+        @unknown default:
+            //Show alert app is too old lol
+            break
+        }
+    }
+    
+    //scale map to user location
+    //Learned here: https://www.youtube.com/watch?v=WPpaAy73nJc
+    func scaleMapToUserLocation() {
+        if let location = manager.location?.coordinate {
+            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionMeter, longitudinalMeters: regionMeter)
+            locationMapView.setRegion(region, animated: true)
+        }
+    }
+    
+    //Update position of person as they move on map
+    //Learned here: https://www.youtube.com/watch?v=WPpaAy73nJc
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //Updating the location of the user on the map
+        guard let location = locations.last else {return}
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionMeter, longitudinalMeters: regionMeter)
+        locationMapView.setRegion(region, animated: true)
+    }
+    
+    //Need to worry about permissions
+    //Learned here: https://www.youtube.com/watch?v=WPpaAy73nJc
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        //Changes authorization
+        checkAppLocationAuthorization()
+    }
+    
+    
+    
+    
     
     func fetchDataFromSearch(){
         if searchInput != ""{
-            let url = URL(string: "https://maps.googleapis.com/maps/api/place/textsearch/json?query=\(newSearchInput)&fields=formatted_address,name,geometry&key=\(APIKey)")
+            let url = URL(string: "https://maps.googleapis.com/maps/api/place/textsearch/json?query=\(newSearchInput)&location=\(locationMapView.annotations[0].coordinate.latitude),\(locationMapView.annotations[0].coordinate.longitude)&radius=\(regionMeter)&fields=formatted_address,name,geometry&key=\(APIKey)")
             do{
                 let data = try Data(contentsOf: url!)
                 theData = try JSONDecoder().decode(APIResults.self, from:data)
@@ -60,10 +181,10 @@ class HomeViewController: UIViewController, UISearchBarDelegate, MKMapViewDelega
         }
         fetchDataFromSearch()
            
-        let displayX: CGFloat = mapView.frame.minX
-        let displayY: CGFloat = mapView.frame.minY
-        let displayWidth: CGFloat = mapView.frame.size.width
-        let displayHeight: CGFloat = mapView.frame.size.height
+        let displayX: CGFloat = locationMapView.frame.minX
+        let displayY: CGFloat = locationMapView.frame.minY
+        let displayWidth: CGFloat = locationMapView.frame.size.width
+        let displayHeight: CGFloat = locationMapView.frame.size.height
 
         myTableView = UITableView(frame: CGRect(x: displayX, y: displayY, width: displayWidth, height: displayHeight))
         myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
@@ -72,8 +193,7 @@ class HomeViewController: UIViewController, UISearchBarDelegate, MKMapViewDelega
         self.view.addSubview(myTableView)
     }
     
-
-
+  
         func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
             if theData != nil {
                 return theData!.results.count
@@ -97,23 +217,28 @@ class HomeViewController: UIViewController, UISearchBarDelegate, MKMapViewDelega
        func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
         if theData != nil {
             print(theData!.results[indexPath.row].geometry?.location ?? 0.0)
-        //performSegue(withIdentifier: "showDetail", sender: nil)
             //let customPinVC: CustomPinViewController = CustomPinViewController()
                   // popupTableViewController.userInputRequest = newSearchInput
 //            customPinVC.chosenSpot = theData!.results[indexPath.row]
            // navigationController?.pushViewController(customPinVC, animated: true)
             
             let storyboard = UIStoryboard(name: "Main", bundle: nil)
-            let linkingVC = storyboard.instantiateViewController(withIdentifier: "createPin")
-            self.navigationController?.pushViewController(linkingVC, animated: true)
+//             let linkingVCFile = NewPinViewController()
+              //  linkingVCFile.newPin = theData!.results[indexPath.row]
+            let linkingVC = storyboard.instantiateViewController(withIdentifier: "newPin") as! NewPinViewController
+            linkingVC.newPin = theData!.results[indexPath.row]
+            present(linkingVC, animated: true)
+           
             
         }
     }
     
     func searchBarCancelButtonClicked(_ search: UISearchBar) {
-        search.text = ""
-        search.showsCancelButton = false
-        myTableView.removeFromSuperview()
+        if myTableView != nil{
+            search.text = ""
+            search.showsCancelButton = false
+            myTableView.removeFromSuperview()
+        }
     }
     
     }
---
+++ Spots/Spots/Controller/NewPinViewController.swift
@@ -0,0 +1,312 @@
+//
+//  NewPinViewController.swift
+//  Spots
+//
+//  Created by Kathryn Krumholz on 12/12/20.
+//  Copyright © 2020 SpotsDevelopers. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import FirebaseStorage
+import FirebaseAuth
+import Firebase
+
+class NewPinViewController: UIViewController, MKMapViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate, CLLocationManagerDelegate  {
+    
+    @IBOutlet weak var pageTitle: UILabel!
+    @IBOutlet weak var locationTitle: UILabel!
+    @IBOutlet weak var previewTitle: UILabel!
+    @IBOutlet weak var addImageButton: UIButton!
+    @IBOutlet weak var finishButton: UIButton!
+    
+    @IBOutlet weak var titleInputField: UITextField!
+    @IBOutlet weak var genreInputField: UITextField!
+    @IBOutlet weak var subtitleTextField: UITextField!
+    
+    @IBOutlet weak var titlePreview: UILabel!
+    @IBOutlet weak var subtitlePreview: UILabel!
+    @IBOutlet weak var imagePreview: UIImageView!
+    
+    @IBOutlet weak var locationMapView: MKMapView!
+    
+    var newPin: Results?
+    var coordinateData: CLLocationCoordinate2D?
+    var currentAnnotation: MKPointAnnotation?
+    
+    let storage = Storage.storage().reference()
+    var imageData: Data?
+    let db = Firestore.firestore()
+    let manager = CLLocationManager()
+    let regionMeter: Double = 10000
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setDefault()
+        checkLocationServices()
+        currentAnnotation = nil
+        coordinateData = nil
+        
+        if newPin != nil{
+            titleInputField.text = newPin!.name
+            if newPin!.geometry != nil{
+                if newPin?.geometry?.location != nil{
+                    coordinateData = CLLocationCoordinate2D(latitude: newPin!.geometry!.location!.lat, longitude: newPin!.geometry!.location!.lng)
+                    let annotation = MKPointAnnotation()
+                    let centerCoordinate = CLLocationCoordinate2D(latitude: newPin!.geometry!.location!.lat, longitude: newPin!.geometry!.location!.lng)
+                    annotation.coordinate = centerCoordinate
+                    annotation.title = "\(newPin!.formatted_address ?? "")"
+                    locationMapView.addAnnotation(annotation)
+                    if coordinateData != nil{
+                        locationMapView.setCenter(coordinateData!, animated: true)
+                    }
+                }
+            }
+        }
+    }
+    
+    //Add styling to all labels, buttons and fields
+    func setDefault () {
+        Utilities.styleLabel(pageTitle)
+        Utilities.styleLabel(locationTitle)
+        Utilities.styleLabel(previewTitle)
+        Utilities.styleButton(addImageButton)
+        Utilities.styleButton(finishButton)
+        Utilities.styleTextFieldAppContent(titleInputField)
+        Utilities.styleTextFieldAppContent(genreInputField)
+        Utilities.styleTextFieldAppContent(subtitleTextField)
+    }
+    
+    
+    //Set up location manager
+    func setUpLocationManager() {
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    //Check if location services are available
+    func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled() {
+            //Set Up location manager
+            setUpLocationManager()
+            checkAppLocationAuthorization()
+        } else {
+            let alert = UIAlertController(title: "Location Service Not Enabled", message: "Please enable location services to improve user experience", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        }
+    }
+    
+    //Check Location services permission for the app
+    func checkAppLocationAuthorization() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            //Show user location on map
+            locationMapView.showsUserLocation = true
+            scaleMapToUserLocation()
+            manager.startUpdatingLocation()
+            break
+        case .denied:
+            //Show alert instructing how to enable them within settings
+            break
+        case .notDetermined:
+            manager.requestWhenInUseAuthorization()
+            break
+        case .restricted:
+            //Show alert that location is restricted
+            break
+        case .authorizedAlways:
+            break
+        @unknown default:
+            //Show alert app is too old lol
+            break
+        }
+    }
+    
+    //scale map to user location
+    //Learned here: https://www.youtube.com/watch?v=WPpaAy73nJc
+    func scaleMapToUserLocation() {
+        if let location = manager.location?.coordinate {
+            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionMeter, longitudinalMeters: regionMeter)
+            locationMapView.setRegion(region, animated: true)
+        }
+    }
+    
+    //Update position of person as they move on map
+    //Learned here: https://www.youtube.com/watch?v=WPpaAy73nJc
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //Updating the location of the user on the map
+        guard let location = locations.last else {return}
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionMeter, longitudinalMeters: regionMeter)
+        locationMapView.setRegion(region, animated: true)
+    }
+    
+    //Need to worry about permissions
+    //Learned here: https://www.youtube.com/watch?v=WPpaAy73nJc
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        //Changes authorization
+        checkAppLocationAuthorization()
+    }
+    
+    //change preview as text field is changed
+    @IBAction func titleTextFieldChanged(_ sender: UITextField) {
+        titlePreview.text = sender.text
+    }
+    
+    //change preview as text field is changed
+    @IBAction func subtitleTextFieldChanged(_ sender: UITextField) {
+        subtitlePreview.text = sender.text
+    }
+    
+    @IBAction func resetDataButtonTapped(_ sender: Any) {
+        let confirm = UIAlertController(title: "Confirm Reset", message: "Are you sure you want to clear the pin in-progress?", preferredStyle: .alert)
+        confirm.addAction(UIAlertAction(title: NSLocalizedString("Yes", comment: "Confirm"), style: .default, handler: { _ in
+            NSLog("Pin clear confirmed, reset VC")
+            self.clearPin(mode: RESET)
+        }))
+        confirm.addAction(UIAlertAction(title: NSLocalizedString("Cancel", comment: "Confirm"), style: .default, handler: { _ in
+            NSLog("Pin clear aborted")
+            return
+        }))
+        self.present(confirm, animated: true, completion: nil)
+    }
+    
+    //sets image to image preview after selecting it from libary
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerEditedImage")] as? UIImage {
+            imagePreview.image = image
+        }
+        
+        guard let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else {
+            return
+        }
+        
+        if image.pngData() != nil {
+            imageData = image.pngData()!
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    //Cancel image picker, no image was selected
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    //add image to preview when image is added
+    //https://www.youtube.com/watch?v=yggOGEzueFk
+    @IBAction func addImageTapped(_ sender: Any) {
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
+    }
+    
+    func clearPin(mode: Int) {
+        if mode == CONFIRM {
+            let genre = genreInputField.text ?? "null_genre"
+            let confirm = UIAlertController(title: "Pin Created", message: "\(titleInputField.text!) was successfully added to your network \(Utilities.parseRecordToDisplayText(record: Utilities.parseInputToRecord(input: genre))).", preferredStyle: .alert)
+            confirm.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Confirm"), style: .default, handler: { _ in
+                NSLog("Pin add confirmed, reset VC")
+            }))
+            self.present(confirm, animated: true, completion: nil)
+        }
+        else if mode == RESET {
+            print("Pin data reset")
+            // DEVNOTE: do anything else? confirm moved to button handler
+        }
+        else {
+            print("Err: invalid mode on clear of custom pin data!")
+        }
+        
+        titleInputField.text = ""
+        genreInputField.text = ""
+        subtitleTextField.text = ""
+        
+        imagePreview.image = nil
+        titlePreview.text = ""
+        subtitlePreview.text = ""
+        
+        coordinateData = nil
+        if currentAnnotation != nil {
+            locationMapView.removeAnnotation(currentAnnotation!)
+            currentAnnotation = nil
+        }
+    }
+    
+    
+    //adds spot to firebase
+    @IBAction func finishTapped(_ sender: Any) {
+        // check if coordinate data exists
+        if coordinateData == nil {
+            let noTitleErrorMsg = UIAlertController(title: "Where was that?", message: "We didn't catch the location of that spot. Press and hold on the map to drop a pin.", preferredStyle: .alert)
+            noTitleErrorMsg.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Go back"), style: .default, handler: { _ in
+                NSLog("Err: no coords on pin save")
+            }))
+            self.present(noTitleErrorMsg, animated: true, completion: nil)
+            return
+        }
+        
+        // check if title exists
+        let title = titleInputField.text ?? ""
+        if title.isEmpty {
+            let noTitleErrorMsg = UIAlertController(title: "Title Required", message: "Every spot need a name. Add something to the title field, then try again.", preferredStyle: .alert)
+            noTitleErrorMsg.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Go back"), style: .default, handler: { _ in
+                NSLog("Err: no title on pin save")
+            }))
+            self.present(noTitleErrorMsg, animated: true, completion: nil)
+            return
+        }
+        
+        //get uid and docid for query
+        let uid = Auth.auth().currentUser?.uid
+        let annotation = locationMapView.annotations
+        var docid = ""
+        var subtitle = ""
+        
+        //check if subtitle exists
+        if (subtitleTextField.text == "") {
+            subtitle = "null"
+        } else {
+            subtitle = subtitleTextField.text!
+        }
+        
+        //add content to database
+        db.collection("spots").addDocument(data: [
+            "uid" : uid!,
+            "title" : titleInputField.text!,
+            "subtitle" : subtitle,
+            "genre_record" : "null", //CHANGE THIS LATER TO BE GENRE RECORD
+            "longitude" : annotation[1].coordinate.longitude,
+            "latitude" : annotation[1].coordinate.latitude
+        ])
+
+        
+        
+        if (imagePreview.image != nil ) {
+            db.collection("spots").whereField("uid", isEqualTo: uid!)
+                .getDocuments() { (querySnapshot, err) in
+                    if let err = err {
+                        print("Error getting documents: \(err)")
+                    } else {
+                        for document in querySnapshot!.documents {
+                            docid = document.documentID
+                            print("docid: \(docid)")
+                            print("\(uid!)/\(docid).png")
+                            self.storage.child("\(uid!)/\(docid).png").putData(self.imageData!, metadata: nil, completion: { _, error in
+                                guard error == nil else {
+                                    print("Failed to Upload")
+                                    return
+                                }
+                            })
+                        }
+                    }
+            }
+        }
+        
+        clearPin(mode: CONFIRM)
+        
+    }
+}
